20952bd9e290fe79de18f0097921d52907d6ccc2
Georg Becker <georg.becker@shellrider.com>
2024-06-23 10:07:39 +0200

    Creates render of tilemap

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,55 @@
 var startTime, lastTimestamp;
+var ctx;
+var display;
+function loadTileSet() {
+    var tileSetImg = new Image();
+    tileSetImg.src = './images/city_tileset.png';
+    return tileSetImg;
+}
+var tileDisplay = {
+    extent: [15, 10],
+    tileSet: loadTileSet(),
+    cellSize: 64,
+    tileSetTileSize: 16,
+    cells: [
+        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+        6, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 6,
+        6, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 6,
+        6, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 6,
+        6, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 6,
+        6, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 6,
+        6, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 6,
+        6, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 6,
+        6, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 6,
+        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+    ]
+};
+function setup() {
+    var canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    display = {
+        width: canvas.width,
+        height: canvas.height
+    };
+    ctx.imageSmoothingEnabled = false;
+}
+function clearCanvas() {
+    if (ctx != undefined && display != undefined) {
+        ctx.clearRect(0, 0, display.width, display.height);
+    }
+}
+function drawMap() {
+    for (var row = 0; row < tileDisplay.extent[1]; row++) {
+        for (var col = 0; col < tileDisplay.extent[0]; col++) {
+            var cellIdx = tileDisplay.cells[col + row * tileDisplay.extent[0]];
+            console.log(cellIdx);
+            var srcCols = tileDisplay.tileSet.width / tileDisplay.tileSetTileSize;
+            ctx.drawImage(tileDisplay.tileSet, (tileDisplay.tileSetTileSize * cellIdx) % tileDisplay.tileSet.width, (tileDisplay.tileSetTileSize * (Math.floor(cellIdx / srcCols))), tileDisplay.tileSetTileSize, tileDisplay.tileSetTileSize, col * tileDisplay.cellSize, row * tileDisplay.cellSize, tileDisplay.cellSize, tileDisplay.cellSize);
+        }
+    }
+}
+function update(delta) {
+}
 function handleFirstFrame(timestamp) {
     startTime = timestamp;
     lastTimestamp = timestamp;
@@ -11,9 +62,12 @@ function gameLoop(timestamp) {
     }
     var delta = (timestamp - lastTimestamp) * 0.001;
     lastTimestamp = timestamp;
-    console.log("Delta: ".concat(delta, "s"));
+    update(delta);
+    clearCanvas();
+    drawMap();
     requestAnimationFrame(gameLoop);
 }
 window.onload = function () {
+    setup();
     requestAnimationFrame(gameLoop);
 };
--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,4 +1,73 @@
 let startTime: number, lastTimestamp: number;
+let ctx: CanvasRenderingContext2D;
+let display;
+
+function loadTileSet(): HTMLImageElement
+{
+    let tileSetImg = new Image();
+    tileSetImg.src = './images/city_tileset.png';
+    return tileSetImg;
+}
+
+const tileDisplay = {
+    extent : [15,10],
+    tileSet : loadTileSet(),
+    cellSize : 64,
+    tileSetTileSize : 16,
+    cells : [
+        6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,
+        6 ,14,14,14,14,14,14,14,14,14,14,14,14,14,6 ,
+        6 ,14,14,14,14,14,14,14,14,14,14,14,14,14,6 ,
+        6 ,14,14,14,14,14,14,14,14,14,14,14,14,14,6 ,
+        6 ,14,14,14,14,14,14,14,14,14,14,14,14,14,6 ,
+        6 ,14,14,14,14,14,14,14,14,14,14,14,14,14,6 ,
+        6 ,14,14,14,14,14,14,14,14,14,14,14,14,14,6 ,
+        6 ,14,14,14,14,14,14,14,14,14,14,14,14,14,6 ,
+        6 ,14,14,14,14,14,14,14,14,14,14,14,14,14,6 ,
+        6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,6 ,
+    ]
+}
+
+function setup() {
+    const canvas : HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("canvas"); 
+    ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
+    display = {
+        width: canvas.width,
+        height: canvas.height
+    };
+    ctx.imageSmoothingEnabled = false;
+}
+
+function clearCanvas(){
+    if(ctx != undefined && display != undefined){
+        ctx.clearRect(0,0, display.width, display.height);
+    }
+}
+
+function drawMap() {
+    for(let row = 0; row < tileDisplay.extent[1]; row++){
+        for(let col = 0; col < tileDisplay.extent[0]; col++){
+            let cellIdx: number = tileDisplay.cells[col + row * tileDisplay.extent[0]];
+            console.log(cellIdx);
+            let srcCols = tileDisplay.tileSet.width / tileDisplay.tileSetTileSize;
+
+            ctx.drawImage(
+                tileDisplay.tileSet,
+                (tileDisplay.tileSetTileSize * cellIdx) % tileDisplay.tileSet.width,
+                (tileDisplay.tileSetTileSize * (Math.floor(cellIdx / srcCols))),
+                tileDisplay.tileSetTileSize,
+                tileDisplay.tileSetTileSize,
+                col * tileDisplay.cellSize,
+                row * tileDisplay.cellSize,
+                tileDisplay.cellSize,
+                tileDisplay.cellSize
+            );
+        }
+    }
+}
+
+function update(delta :number){
+}
 
 function handleFirstFrame(timestamp: number) {
     startTime = timestamp;
@@ -13,10 +82,15 @@ function gameLoop(timestamp: number) {
     }
     const delta: number = (timestamp - lastTimestamp) * 0.001;
     lastTimestamp = timestamp;
-    console.log(`Delta: ${delta}s`);
+
+    update(delta);
+    clearCanvas();
+    drawMap();
+
     requestAnimationFrame(gameLoop);
 }
 
 window.onload = () => {
+    setup();
     requestAnimationFrame(gameLoop);
 }
