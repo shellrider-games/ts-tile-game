0b76c5cbd6dae20703440267f41226e98d381bfb
Georg Becker <georg.becker@shellrider.com>
2024-06-23 08:37:37 +0200

    Sets up basic game loop

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,19 @@
-var message = 'Hello, World!';
-console.log(message);
+var startTime, lastTimestamp;
+function handleFirstFrame(timestamp) {
+    startTime = timestamp;
+    lastTimestamp = timestamp;
+    requestAnimationFrame(gameLoop);
+}
+function gameLoop(timestamp) {
+    if (startTime === undefined) {
+        handleFirstFrame(timestamp);
+        return;
+    }
+    var delta = (timestamp - lastTimestamp) * 0.001;
+    lastTimestamp = timestamp;
+    console.log("Delta: ".concat(delta, "s"));
+    requestAnimationFrame(gameLoop);
+}
+window.onload = function () {
+    requestAnimationFrame(gameLoop);
+};
--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,2 +1,22 @@
-let message: string = 'Hello, World!';
-console.log(message);
+let startTime: number, lastTimestamp: number;
+
+function handleFirstFrame(timestamp: number) {
+    startTime = timestamp;
+    lastTimestamp = timestamp;
+    requestAnimationFrame(gameLoop);
+}
+
+function gameLoop(timestamp: number) {
+    if (startTime === undefined){
+        handleFirstFrame(timestamp);
+        return;
+    }
+    const delta: number = (timestamp - lastTimestamp) * 0.001;
+    lastTimestamp = timestamp;
+    console.log(`Delta: ${delta}s`);
+    requestAnimationFrame(gameLoop);
+}
+
+window.onload = () => {
+    requestAnimationFrame(gameLoop);
+}
